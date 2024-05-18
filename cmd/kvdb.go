d64b8cb370297535f87f049895cfe4a77ff26b5c
zeidlitz <zeidlitzerik@gmail.com>
2024-05-18 12:21:41 +0200

    Entrypoint, logging, config, gitignore

--- cmd/kvdb.go
+++ cmd/kvdb.go
@@ -1,7 +1,21 @@
 package main
 
-import("fmt")
+import(
+  "fmt"
+  "log/slog"
+
+  "github.com/zeidlitz/kvdb/internal/env"
+)
+
+type Config struct{
+  serverAddress string
+  httpPort int
+}
 
 func main() {
-  fmt.Println("Starting up")
+  var cfg Config
+  cfg.serverAddress = env.GetString("SERVER_ADDRESS", "localhost")
+  cfg.httpPort = env.GetInt("HTTP_PORT", 8080)
+  address := fmt.Sprintf(cfg.serverAddress + ":" + "%d", cfg.httpPort)
+  slog.Info("Starting up", "address", address)
 }
